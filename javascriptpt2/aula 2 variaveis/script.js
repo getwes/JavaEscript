0468a8558b24f2c0127ffec3e5b25b4f30565d8c
getwes <wesleyfreire1707@gmail.com>
2021-11-17 23:16:21 -0300

    tipos primitivos

--- javascriptpt2/aula 2 variaveis/script.js
+++ javascriptpt2/aula 2 variaveis/script.js
@@ -1,13 +1,18 @@
 // tipos primitivos
 
 //boolean
-var V0uF = false;
-console.log(typeof(V0uF));
+//var V0uF = false;
+//console.log(typeof(V0uF));
 
 //number
-var numeroqualquer = 1;
-console.log(typeof(numeroqualquer));
+//var numeroqualquer = 1;
+//console.log(typeof(numeroqualquer));
 
 //string
-var nome = 'wesley';
-console.log(typeof(nome))
+//var nome = 'wesley';
+//console.log(typeof(nome))
+
+// como declarar
+var variavel = 'wesley';
+variave = 'freire';
+console.log(variavel);
